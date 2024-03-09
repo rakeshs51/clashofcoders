e068ce6a1e2e50a2096a7f77666c18fd068f2711
Rakesh Sangolli <55094915+rakeshs51@users.noreply.github.com>
2024-03-09 23:34:32 +0530

    Update ProblemsTable.tsx
    Fixing solved problems

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -8,9 +8,8 @@ import { collection, doc, getDoc, getDocs, orderBy, query } from "firebase/fires
 import { auth, firestore } from "@/firebase/firebase";
 import { DBProblem } from "@/utils/types/problem";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { problems } from "@/mockProblems/problems";
 
-type ProblemsTableProps = { 
+type ProblemsTableProps = {
 	setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
@@ -48,12 +47,10 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => 
 					return (
 						<tr className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`} key={problem.id}>
 							<th className='px-2 py-4 font-medium whitespace-nowrap text-dark-green-s'>
-								{/* {solvedProblems.includes(problem.id) &&  */}
-								<BsCheckCircle fontSize={"18"} width='18' />
-								{/* // } */}
+								{solvedProblems.includes(problem.id) && <BsCheckCircle fontSize={"18"} width='18' />}
 							</th>
 							<td className='px-6 py-4'>
-								{/* {problem.link ? (
+								{problem.link ? (
 									<Link
 										href={problem.link}
 										className='hover:text-blue-600 cursor-pointer'
@@ -61,14 +58,14 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => 
 									>
 										{problem.title}
 									</Link>
-								) : ( */}
+								) : (
 									<Link
 										className='hover:text-blue-600 cursor-pointer'
 										href={`/problems/${problem.id}`}
 									>
 										{problem.title}
 									</Link>
-								{/* )} */}
+								)}
 							</td>
 							<td className={`px-6 py-4 ${difficulyColor}`}>{problem.difficulty}</td>
 							<td className={"px-6 py-4"}>{problem.category}</td>
